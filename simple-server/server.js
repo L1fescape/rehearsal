6acdd7ca8b71023d627a1727104ea52d4e50f88b
Andrew Kennedy <akennedy2292@gmail.com>
2012-11-16 18:16:34 -0500

    messing around with websockets and nodejs chat

--- simple-server/server.js
+++ simple-server/server.js
@@ -1,6 +1,17 @@
 var http = require('http');
+
 http.createServer(function (req, res) {
-	  res.writeHead(200, {'Content-Type': 'text/plain'});
-		  res.end('Hello World\n');
+	var body = 'Hello World\n';
+
+	res.writeHead(200, {
+		'Content-Type': 'text/plain',
+		'Content-Length': body.length,
+		'Set-Cookie': ["type=ninja", "language=javascript"]
+	});
+
+
+	res.end(body);
+
 }).listen(1337, "127.0.0.1");
+
 console.log('Server running at http://127.0.0.1:1337/');
---
+++ socket-test/readme.markdown
@@ -0,0 +1,1 @@
+Source: http://martinsikora.com/nodejs-and-websocket-simple-chat-tutorial

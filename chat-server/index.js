1faed16d4433383a22a4b7789119a9752f19fdf0
Andrew Kennedy <akennedy2292@gmail.com>
2013-01-18 02:39:16 -0500

    added really basic chat server

---
+++ chat-server/index.js
@@ -0,0 +1,43 @@
+var	app = require('http').createServer(handler),
+     io = require('socket.io').listen(app),
+     fs = require('fs')
+		 url = require('url');
+
+app.listen(1337);
+console.log("Listening on port 1337")
+
+function handler (req, res) {
+	var pathname = url.parse(req.url).pathname;
+	if (pathname == "/") {
+		fs.readFile(__dirname + '/index.html',
+		function (err, data) {
+			if (err) {
+				res.writeHead(500);
+				return res.end('Error loading index.html');
+			}
+
+			res.writeHead(200);
+			res.end(data);
+		});
+	}
+	else if (pathname == "/jquery.js") {
+		fs.readFile(__dirname + '/jquery.js',
+		function (err, data) {
+			if (err) {
+				res.writeHead(500);
+				return res.end('Error loading jquery.js');
+			}
+
+			res.writeHead(200);
+			res.end(data);
+		});
+	}
+}
+
+io.sockets.on('connection', function (socket) {
+	socket.emit('recieve', { hello: 'world' });
+	socket.on('send', function (data) {
+		console.log(data);
+		io.sockets.emit('receive', data);
+	});
+});
---
+++ chat-server/readme.md
@@ -0,0 +1,12 @@
+Simple Chat Server
+==================
+
+via socket.io
+Expanded upon demo from here: http://socket.io/#how-to-use
+
+Dependencies
+* Socket.io
+
+	<code>npm install socket.io</code>
+
+

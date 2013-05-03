d024a68dbd30760c877add0c91ede856d1d7f802
Andrew Kennedy <akennedy2292@gmail.com>
2013-05-03 08:13:20 -0400

    added web server

--- clojure/hello-world/project.clj
+++ clojure/hello-world/project.clj
@@ -3,5 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [compojure "1.1.5"]
+                 [ring "1.0.2"]]
+  :plugins [[lein-ring "0.8.5"]]
+  :ring {:handler hello-world.core/app}
   :main hello-world.core)
--- clojure/hello-world/src/hello_world/core.clj
+++ clojure/hello-world/src/hello_world/core.clj
@@ -1,7 +1,7 @@
 (ns hello-world.core
-  (:gen-class))
+  (:use compojure.core)
+  (:require [compojure.route :as route]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defroutes app
+  (GET "/" [] "<h1>Hello World</h1>")
+  (route/not-found "<h1>Page not found</h1>"))

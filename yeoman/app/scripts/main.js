3f0ca77861e951dedb93d670d4505152ae4a341f
Andrew Kennedy <akennedy2292@gmail.com>
2013-05-18 00:44:17 -0700

    added yeoman tests

---
+++ yeoman/app/scripts/main.js
@@ -0,0 +1,17 @@
+/*global yo, $*/
+'use strict';
+
+
+window.yo = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    yo.init();
+});

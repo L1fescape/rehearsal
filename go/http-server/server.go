0d8a8979d686397b5529fe54b686805b5bb3afe0
Andrew Kennedy <akennedy2292@gmail.com>
2014-02-06 18:36:00 -0500

    first iteration of http server with post variable parsing

---
+++ go/http-server/server.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+  "net"
+  "fmt"
+  "strings"
+  "bytes"
+  "encoding/json"
+)
+
+type Person struct {
+  Id int `json:",string"`
+  Name string
+}
+
+func parseResponse(request []byte) []string {
+  req := bytes.Split(request, []byte("\n"))
+  reqInfo := strings.Split(string(req[0]), " ")
+
+  method := reqInfo[0]
+  //url := reqInfo[1]
+  var body []byte = req[len(req) - 1]
+
+  fmt.Println(string(body))
+
+  resp := []string{"HTTP/1.1 200 OK\r\n\r\n"}
+
+  switch method {
+    case "GET":
+      resp = append(resp, "Hi there!")
+    case "POST":
+      var person Person
+      err := json.Unmarshal(body, &person)
+      if err != nil { panic(err) }
+      resp = append(resp, "Hi there ")
+      resp = append(resp, person.Name)
+      resp = append(resp, "!")
+  }
+  return resp
+}
+
+func handleConnection(conn net.Conn) {
+  // try to read data from the connection
+  data := make([]byte, 512)
+  n, err := conn.Read(data)
+  if err != nil { panic(err)  }
+  cdata := data[:n]
+  //s := string(data[:n])
+  //fmt.Println(s)
+  resp := parseResponse(cdata)
+
+  // send a response
+  var x = []byte{}
+  // convert string array to byte array so it can
+  // be written to the connection
+  for i:=0; i<len(resp); i++{
+    b := []byte(resp[i])
+    for j:=0; j<len(b); j++{
+      x = append(x,b[j])
+    }
+  }
+  // write the data to the connection
+  _, err = conn.Write(x)
+  if err != nil { panic(err) }
+  // close the connection
+  conn.Close()
+}
+
+func main() {
+  ln, err := net.Listen("tcp", ":8081")
+  if err != nil {
+    // handle error
+  }
+  for {
+    conn, err := ln.Accept()
+    if err != nil {
+      // handle error
+      continue
+    }
+    go handleConnection(conn)
+  }
+}

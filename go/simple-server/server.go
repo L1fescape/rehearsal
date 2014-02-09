eb4b2fc9292e40e6d3785a3787b8820f52e95d44
Andrew Kennedy <akennedy2292@gmail.com>
2014-02-08 19:00:09 -0500

    simplified simple server script

--- go/simple-server/server.go
+++ go/simple-server/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
   "net"
+  "strings"
   "fmt"
 )
 
@@ -11,23 +12,14 @@ func handleConnection(conn net.Conn) {
   n, err := conn.Read(data)
   if err != nil { panic(err)  }
   s := string(data[:n])
-  
+
   // print the request data
   fmt.Println(s)
 
   // send a response
-  var str = []string{"HTTP/1.1 200 OK\r\n\r\n"}
-  var x = []byte{}
-  // convert string array to byte array so it can
-  // be written to the connection
-  for i:=0; i<len(str); i++{
-    b := []byte(str[i])
-    for j:=0; j<len(b); j++{
-      x = append(x,b[j])
-    }
-  }
-  // write the data to the connection
-  _, err = conn.Write(x)
+  var resp = []string{"Hi", "there!"}
+  var str = strings.Join(resp, " ")
+  _, err = conn.Write([]byte(str))
   if err != nil { panic(err) }
   // close the connection
   conn.Close()
